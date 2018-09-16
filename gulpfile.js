20c54fe12740ae34211e4cbaf5659dfbaa381af8
Chris Dickson <cdickson@uncharted.software>
2018-09-16 11:53:43 -0400

    try throwing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const fail = require('gulp-fail');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
+  let failed = false;
   return gulp.src('src/**/*.js', {follow: true})
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.format())
+    .pipe(eslint.result((result) => {
+      if (result.errorCount > 0 || result.warningCount > 0) {
+        throw {
+          name: 'ESLintError',
+          message: 'Lint errors/warnings:'
+        };
+      }
+    }));
 });
--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@
     return a+b;
   };
 
+  const modify = (a,b) => {
+    return {a: a,b};
+  };
+
   sum(5,2);
+  modify(5,2);
 })();
